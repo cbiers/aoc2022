e960efa25f0f2f0b0a150865b618e2d8d23c9bf2
cbiers <chris.biersbach@gmail.com>
2022-12-05 09:40:43 +0100

    Day 5 done

---
+++ 5/1.py
@@ -0,0 +1,25 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+
+stacks = [
+    ["P", "F", "M", "Q", "W", "G", "R", "T"],
+    ["H", "F", "R"],
+    ["P", "Z", "R", "V", "G", "H", "S", "D"],
+    ["Q", "H", "P", "B", "F", "W", "G"],
+    ["P", "S", "M", "J", "H"],
+    ["M", "Z", "T", "H", "S", "R", "P", "L"],
+    ["P", "T", "H", "N", "M", "L"],
+    ["F", "D", "Q", "R"],
+    ["D", "S", "C", "N", "L", "P", "H"]
+]
+
+for line in lines:
+    s = line.split()
+    n = int(s[1])
+    s1 = int(s[3]) - 1
+    s2 = int(s[5]) - 1
+    for i in range(n):
+        stacks[s2].append(stacks[s1].pop())
+
+for stack in stacks:
+    print stack[-1]
