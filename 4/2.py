a609ac0e60536db73323d000c787b9e428ee41ab
cbiers <chris.biersbach@gmail.com>
2022-12-05 08:50:52 +0100

    Day 4 done

---
+++ 4/2.py
@@ -0,0 +1,13 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+
+count = 0
+
+for line in lines:
+    pairs = line[:-1].split(",")
+    pair1 = pairs[0].split("-")
+    pair2 = pairs[1].split("-")
+    if (int(pair1[0]) <= int(pair2[0]) and int(pair1[1]) >= int(pair2[0])) or (int(pair2[0]) <= int(pair1[0]) and int(pair2[1]) >= int(pair1[0])):
+        count += 1
+
+print count
