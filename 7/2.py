da5b12948fc81cb65f6bf9c7d618c6ad9816f8c8
cbiers <chris.biersbach@gmail.com>
2022-12-08 10:55:31 +0100

    Day 7 done

---
+++ 7/2.py
@@ -0,0 +1,88 @@
+f = open("input.txt", "r")
+lines = f.readlines()
+
+class File:
+
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+
+    def __str__(self):
+        return self.name + " (" + str(self.size) + ")"
+
+class Directory:
+
+    def __init__(self, name):
+        self.name = name
+        self.files = []
+        self.dirs = []
+        self.parent = None
+
+    def addFile(self, file):
+        self.files.append(file)
+
+    def addDir(self, dir):
+        self.dirs.append(dir)
+        dir.parent = self
+
+    def totalSize(self):
+        total = 0
+        for file in self.files:
+            total += file.size
+        for dir in self.dirs:
+            total += dir.totalSize()
+        return total
+
+    def dirsWithMinSize(self, size, l):
+        ownSize = self.totalSize()
+        if ownSize >= size:
+            l.append(ownSize)
+        for dir in self.dirs:
+            dir.dirsWithMinSize(size, l)
+
+    def findDir(self, name):
+        for dir in self.dirs:
+            if dir.name == name:
+                return dir
+
+    def findFile(self, name):
+        for file in self.files:
+            if file.name == name:
+                return file
+
+    def __str__(self):
+        res = self.name + "\n"
+        for file in self.files:
+            res += "-" + str(file) + "\n"
+        for dir in self.dirs:
+            res += "d" + str(dir) + "\n"
+        return res
+
+
+root = Directory("/")
+current = root
+
+for line in lines:
+    parts = line[:-1].split()
+    if parts[0] == "$":
+        if parts[1] == "cd":
+            if parts[2] == "/":
+                current = root
+            elif parts[2] == "..":
+                current = current.parent
+            else:
+                current = current.findDir(parts[2])
+    else:
+        if parts[0] == "dir":
+            if not current.findDir(parts[1]):
+                current.addDir(Directory(parts[1]))
+        else:
+            if not current.findFile(parts[1]):
+                current.addFile(File(parts[1], int(parts[0])))
+
+rootSize = root.totalSize()
+remaining = 70000000 - rootSize
+needed = 30000000 - remaining
+l = []
+root.dirsWithMinSize(needed, l)
+print min(l)
